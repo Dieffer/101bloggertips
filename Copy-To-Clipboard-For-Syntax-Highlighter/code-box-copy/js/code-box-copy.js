f94133a8a5bce76ccd15932af490b0b29594cd29
Dieffer <37056584+Dieffer@users.noreply.github.com>
2018-07-09 11:10:35 +0800

    Update code-box-copy.js

--- Copy-To-Clipboard-For-Syntax-Highlighter/code-box-copy/js/code-box-copy.js
+++ Copy-To-Clipboard-For-Syntax-Highlighter/code-box-copy/js/code-box-copy.js
@@ -1,1 +1,62 @@
-!function(p,t){var i="codeBoxCopy",e={tooltipText:"Copied",tooltipShowTime:1e3,tooltipFadeInTime:300,tooltipFadeOutTime:300};function o(t,o){this.element=t,this.options=p.extend({},e,o),this._defaults=e,this._name=i,this.init()}o.prototype={init:function(){var o,i,t,e,n;(t=this.element.querySelector(".code-box-copy__btn"))&&(n=this.options,new Clipboard(t).on("success",function(t){(o=p(t.trigger)).prop("disabled",!0),e='<span class="code-box-copy__tooltip">',e+=n.tooltipText,p(e+="</span>").prependTo(o),(i=o.find(".code-box-copy__tooltip")).fadeIn(n.tooltipFadeInTime),setTimeout(function(){i.fadeOut(n.tooltipFadeOutTime,function(){i.remove()}),o.prop("disabled",!1)},n.tooltipShowTime)}))}},p.fn[i]=function(t){return this.each(function(){p.data(this,"plugin_"+i)||p.data(this,"plugin_"+i,new o(this,t))})}}(jQuery);
+;
+(function ($, undefined) {
+
+	var pluginName = 'codeBoxCopy',
+		defaults = {
+			tooltipText: 'Copied',
+			tooltipShowTime: 1000,
+			tooltipFadeInTime: 300,
+			tooltipFadeOutTime: 300
+		};
+
+	function Plugin(element, options) {
+		this.element = element;
+		this.options = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
+
+	Plugin.prototype = {
+
+		init: function () {
+
+			var $btn, $tooltip, btn, tooltip, opts, clipboard;
+
+			btn = this.element.querySelector('.code-box-copy__btn');
+
+			if (!btn) return;
+
+			opts = this.options;
+			clipboard = new Clipboard(btn);
+
+			clipboard.on('success', function (e) {
+				$btn = $(e.trigger);
+				$btn.prop('disabled', true);
+
+				tooltip = '<span class="code-box-copy__tooltip">';
+				tooltip += opts.tooltipText;
+				tooltip += '</span>';
+				$(tooltip).prependTo($btn);
+				$tooltip = $btn.find('.code-box-copy__tooltip');
+				$tooltip.fadeIn(opts.tooltipFadeInTime);
+
+				setTimeout(function () {
+					$tooltip.fadeOut(opts.tooltipFadeOutTime, function () {
+						$tooltip.remove();
+					});
+					$btn.prop('disabled', false);
+				}, opts.tooltipShowTime);
+			});
+		}
+	};
+
+	$.fn[pluginName] = function (options) {
+		return this.each(function () {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName,
+					new Plugin(this, options));
+			}
+		});
+	};
+})(jQuery);
